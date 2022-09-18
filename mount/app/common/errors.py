313a485ae05ac5a34d012fcb2c9b5e2d53535d2d
cmyui <cmyuiosu@gmail.com>
2022-09-18 14:10:54 -0400

    feat(V2-80): usecases for chat members

--- mount/app/common/errors.py
+++ mount/app/common/errors.py
@@ -8,3 +8,8 @@ class ServiceError(str, Enum):
     CHATS_CANNOT_UPDATE = 'chats.cannot_update'
     CHATS_CANNOT_DELETE = 'chats.cannot_delete'
     CHATS_NOT_FOUND = 'chats.not_found'
+
+    MEMBERS_CANNOT_CREATE = 'members.cannot_create'
+    MEMBERS_CANNOT_UPDATE = 'members.cannot_update'
+    MEMBERS_CANNOT_DELETE = 'members.cannot_delete'
+    MEMBERS_NOT_FOUND = 'members.not_found'
