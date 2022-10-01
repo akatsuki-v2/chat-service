4d065b7db0c97dfcc1e4826635e657c93188309b
cmyui <cmyuiosu@gmail.com>
2022-09-30 23:05:13 -0400

    refactor: psql to mysql

--- mount/database/migrations/000002_create_chats_table.up.sql
+++ mount/database/migrations/000002_create_chats_table.up.sql
@@ -1,14 +1,12 @@
 CREATE TABLE chats (
-    chat_id SERIAL NOT NULL PRIMARY KEY,
+    chat_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name TEXT NOT NULL,
     topic TEXT NOT NULL,
     read_privileges INT NOT NULL,
     write_privileges INT NOT NULL,
     auto_join BOOLEAN NOT NULL,
     status VARCHAR(64) NOT NULL,
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created_by INT NOT NULL
+    updated_at DATETIME NOT NULL DEFAULT NOW(),
+    created_at DATETIME NOT NULL DEFAULT NOW(),
+    created_by INT NOT NULL COMMENT 'reference to accounts.id'
 );
-
-COMMENT ON COLUMN chats.created_by IS 'reference to accounts.id';
